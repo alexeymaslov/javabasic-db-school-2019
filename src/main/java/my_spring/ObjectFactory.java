0762648fe608121e8a278fdf1750550c92c6bcc2
jeka1978 <kit2009>
2019-08-23 15:01:38 +0300

    write support for InjectRandomInt done

--- src/main/java/my_spring/ObjectFactory.java
+++ src/main/java/my_spring/ObjectFactory.java
@@ -1,8 +1,14 @@
 package my_spring;
 
+import design_patterns.strategy.heroes.RandomUtil;
 import lombok.SneakyThrows;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
 
-import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Evgeny Borisov
@@ -10,39 +16,43 @@ import java.lang.invoke.SerializedLambda;
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
     private Config config = new JavaConfig();
+    private List<ObjectConfigurator> configurators = new ArrayList<>();
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
 
+    @SneakyThrows
     private ObjectFactory() {
+        Reflections scanner = new Reflections("my_spring");
+        Set<Class<? extends ObjectConfigurator>> classes = scanner.getSubTypesOf(ObjectConfigurator.class);
+        for (Class<? extends ObjectConfigurator> aClass : classes) {
+            configurators.add(aClass.getDeclaredConstructor().newInstance());
+        }
     }
 
     @SneakyThrows
     public <T> T createObject(Class<T> type) {
-        if (type.isInterface()) {
-            type = config.getImplClass(type);
-        }
-        T t = type.getDeclaredConstructor().newInstance();
-//todo
-
+        type = resolveImpl(type);
+        T t = create(type);
+        configure(t);
         return t;
     }
 
+    private <T> void configure(T t) {
+        configurators.forEach(configurator -> configurator.configure(t));
+    }
 
+    private <T> T create(Class<T> type) throws InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException, NoSuchMethodException {
+        return type.getDeclaredConstructor().newInstance();
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    private <T> Class<T> resolveImpl(Class<T> type) {
+        if (type.isInterface()) {
+            type = config.getImplClass(type);
+        }
+        return type;
+    }
 
 
 }
