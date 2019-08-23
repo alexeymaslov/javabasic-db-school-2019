24644ea5b71c840e8df39b624c6476998809ce25
jeka1978 <kit2009>
2019-08-23 12:46:53 +0300

    my spring beginning

--- src/main/java/my_spring/IRobot.java
+++ src/main/java/my_spring/IRobot.java
@@ -1,19 +1,22 @@
 package my_spring;
 
 import java.rmi.dgc.DGC;
+import java.util.Map;
 
 /**
  * @author Evgeny Borisov
  */
 public class IRobot {
 
-    private Speaker speaker = new ConsoleSpeaker();
-    private Cleaner cleaner = new CleanerImpl();
+    private Speaker speaker = ObjectFactory.getInstance().createObject(Speaker.class);
+    private Cleaner cleaner = ObjectFactory.getInstance().createObject(Cleaner.class);
 
     public void cleanRoom() {
         speaker.speak("I started...");
         cleaner.clean();
         speaker.speak("I finished...");
+
+
 
     }
 }
---
+++ src/main/java/my_spring/ObjectFactory.java
@@ -0,0 +1,48 @@
+package my_spring;
+
+import lombok.SneakyThrows;
+
+import java.lang.invoke.SerializedLambda;
+
+/**
+ * @author Evgeny Borisov
+ */
+public class ObjectFactory {
+    private static ObjectFactory ourInstance = new ObjectFactory();
+    private Config config = new JavaConfig();
+
+    public static ObjectFactory getInstance() {
+        return ourInstance;
+    }
+
+    private ObjectFactory() {
+    }
+
+    @SneakyThrows
+    public <T> T createObject(Class<T> type) {
+        if (type.isInterface()) {
+            type = config.getImplClass(type);
+        }
+        T t = type.getDeclaredConstructor().newInstance();
+//todo
+
+        return t;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
