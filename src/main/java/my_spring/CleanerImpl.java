1f0864e29166ee86980123a1915d07079e5a903d
jeka1978 <kit2009>
2019-08-23 13:59:08 +0300

    write support for InjectRandomInt

--- src/main/java/my_spring/CleanerImpl.java
+++ src/main/java/my_spring/CleanerImpl.java
@@ -4,8 +4,13 @@ package my_spring;
  * @author Evgeny Borisov
  */
 public class CleanerImpl implements Cleaner {
+    @InjectRandomInt(min=3,max=7)
+    private int repeat;
+
     @Override
     public void clean() {
-        System.out.println("VVVVVVVVVVVVVvvvvvvvvvvvvv");
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("VVVVVVVVVVVVVvvvvvvvvvvvvv");
+        }
     }
 }
